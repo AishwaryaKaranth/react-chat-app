a9003cb357e461f3aa3389c16f8b5e8021f18c0e
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-05-03 19:21:49 +0530

    firebase config

---
+++ chat-app/src/firebase_config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBBrpNgjwRO1lkrUv4KkTt6KwAZmlw0GZQ",
+    authDomain: "chat-app-f974b.firebaseapp.com",
+    projectId: "chat-app-f974b",
+    storageBucket: "chat-app-f974b.appspot.com",
+    messagingSenderId: "540095935555",
+    appId: "1:540095935555:web:94abb0d8c9ab3ee62bd0f6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
